fcccb9a7a23b504271c973bdf1daea8ff7f73cf6
Jonathan Hickford <jonathan.hickford@red-gate.com>
2016-06-16 17:01:39 +0100

    sdfsdfsd

--- Stored Procedures/dbo.prcGetContacts.sql
+++ Stored Procedures/dbo.prcGetContacts.sql
@@ -6,12 +6,6 @@ CREATE PROCEDURE [dbo].[prcGetContacts]
 AS
     SELECT  ContactsID,
             ContactFullName,
-            PhoneWork,
-            PhoneMobile,
-            Address1,
-            Address2,
-            Address3,
-            CountryCode,
             JoiningDate,
             ModifiedDate,
             Email,
